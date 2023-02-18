97c05b342ddc0ae34e6a6681f4f9c6ce8a58803a
Joel Tchoupe <73880183+jojocam666@users.noreply.github.com>
2023-02-18 10:27:35 +0100

    Update and rename transaction to transaction.py

---
+++ skeleton/blockchain/transaction.py
@@ -0,0 +1,4 @@
+#Une transaction est composée de N inputs et de N Outputs. N est un entier naturel. Cela signifie que l’on ne peut pas utiliser une demi-transaction.
+#Une transaction possède l’adresse du destinataire
+#Une transaction utilise un Token comme unité
+#Valider une transaction demande des frais de fonctionnement, ils sont nommés Gaz
