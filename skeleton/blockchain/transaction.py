a5582294a88d5af80da5a3a544c84ab1a923b5c8
Joel Tchoupe <73880183+jojocam666@users.noreply.github.com>
2023-02-18 10:31:17 +0100

    Update transaction.py

--- skeleton/blockchain/transaction.py
+++ skeleton/blockchain/transaction.py
@@ -2,3 +2,14 @@
 #Une transaction possède l’adresse du destinataire
 #Une transaction utilise un Token comme unité
 #Valider une transaction demande des frais de fonctionnement, ils sont nommés Gaz
+#1. Création – Joe effectue une transaction pour Jane
+
+#2. Compilation – Plusieurs transactions sont regroupées dans un bloc
+
+#3. Consensus – Compétition ou vote pour valider son bloc
+
+#4. Validation – Le bloc est validé par la communauté
+
+#5. Diffusion – Le bloc est diffusé à tous les nœuds
+
+#6. Réalisation – Jane reçoit sa transaction & le propriétaire du bloc validé reçoit une récompense
